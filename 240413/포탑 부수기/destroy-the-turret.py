594a051bf1d714bb2f605d622e7b3abe59387040
zigimi <99969990+zigimi@users.noreply.github.com>
2024-04-13 00:43:49 +0900

    Title: 포탑 부수기, Time: 196ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240413/포탑 부수기/destroy-the-turret.py
+++ 240413/포탑 부수기/destroy-the-turret.py
@@ -1,5 +1,5 @@
 import copy
-import heapq
+from collections import deque
 n, m, k = map(int, input().split())
 mymap = []
 # attackpriority = []
@@ -29,6 +29,44 @@ def dfs(i, j, dx, dy, dist, att, route):
         dfs((i-1+n) % n, j, dx, dy, dist+1, att, route)
         route.pop()
         check[i][j] = False
+
+def bfs(i, j, dx, dy):
+    global minroute
+    q = deque()
+    q.append([[i, j]])
+    check[i][j] = True
+    while q:
+        tmp = q.popleft()
+        if mymap[tmp[-1][0]][tmp[-1][1]] == 0:
+            continue
+
+        if tmp[-1][0] == dx and tmp[-1][1] == dy:
+            minroute = copy.deepcopy(tmp)
+            break
+
+        i = tmp[-1][0]
+        j = tmp[-1][1]
+
+        if check[i][(j+1)%m] == False:
+            check[i][(j + 1) % m] = True
+            tmptmp = copy.deepcopy(tmp)
+            tmptmp.append([i, (j + 1) % m])
+            q.append(tmptmp)
+        if check[(i+1)%n][j] == False:
+            check[(i + 1) % n][j] = True
+            tmptmp = copy.deepcopy(tmp)
+            tmptmp.append([(i+1)%n, j])
+            q.append(tmptmp)
+        if check[i][(j-1+m)%m] == False:
+            check[(i + 1) % n][j] = True
+            tmptmp = copy.deepcopy(tmp)
+            tmptmp.append([i, (j-1+m)%m])
+            q.append(tmptmp)
+        if check[(i-1+n) % n][j] == False:
+            check[(i - 1 + n) % n][j] = True
+            tmptmp = copy.deepcopy(tmp)
+            tmptmp.append([(i-1+n) % n, j])
+            q.append(tmptmp)
 
 for i in range(k):
     if len(recent) == 1:
@@ -74,40 +112,40 @@ for i in range(k):
     tmpedx = -1
     tmpedy = -1
     maxattack = 0
-    for j in range(n):
-        for k in range(m):
-            if mymap[j][k] > 0 and not (j == tmpx and k == tmpy):
-                if mymap[j][k] > maxattack:
+    for j, k in recent.keys():
+        if mymap[j][k] > 0 and not (j == tmpx and k == tmpy):
+            if mymap[j][k] > maxattack:
+                maxattack = mymap[j][k]
+                tmpedx = j
+                tmpedy = k
+            elif maxattack == mymap[j][k]:
+                if tmpedx == tmpedy == -1 or recent[(j, k)] < recent[(tmpedx, tmpedy)]:
                     maxattack = mymap[j][k]
                     tmpedx = j
                     tmpedy = k
-                elif maxattack == mymap[j][k]:
-                    if tmpedx == tmpedy == -1 or recent[(j, k)] < recent[(tmpedx, tmpedy)]:
+                elif recent[(j, k)] == recent[(tmpedx, tmpedy)]:
+                    if j + k < tmpedx + tmpedy:
                         maxattack = mymap[j][k]
                         tmpedx = j
                         tmpedy = k
-                    elif recent[(j, k)] == recent[(tmpedx, tmpedy)]:
-                        if j + k < tmpedx + tmpedy:
+                    elif j + k == tmpedx + tmpedy:
+                        if k < tmpedy:
                             maxattack = mymap[j][k]
                             tmpedx = j
                             tmpedy = k
-                        elif j + k == tmpedx + tmpedy:
-                            if k < tmpedy:
-                                maxattack = mymap[j][k]
-                                tmpedx = j
-                                tmpedy = k
     attacked.append([tmpedx, tmpedy])
     # 레이저 공격
     mindist = n*m+1
     check = [[False for _ in range(m)] for _ in range(n)]
     minroute = []
     att = mymap[tmpx][tmpy]//2
-    dfs(tmpx, tmpy, tmpedx, tmpedy, 0, att, [])
+    # dfs(tmpx, tmpy, tmpedx, tmpedy, 0, att, [])
+    bfs(tmpx, tmpy, tmpedx, tmpedy)
     # 공격
     mymap[tmpedx][tmpedy] = max(mymap[tmpedx][tmpedy] - mymap[tmpx][tmpy], 0)
     # 레이저
     if minroute:
-        for j in range(1, len(minroute)):
+        for j in range(1, len(minroute)-1):
             mymap[minroute[j][0]][minroute[j][1]] = max(mymap[minroute[j][0]][minroute[j][1]]-att, 0)
             attacked.append(minroute[j])
     # 포탄
@@ -128,12 +166,12 @@ for i in range(k):
         attacked.append([(tmpedx+1)%n, tmpedy])
         attacked.append([(tmpedx+1)%n, (tmpedy-1+m)%m])
         attacked.append([tmpedx, (tmpedy-1+m)%m])
-        
+
     # print(minroute)
     # for j in range(n):
     #     print(mymap[j])
     # print()
-    
+
     keys = list(recent.keys())
     for j, k in keys:
             if mymap[j][k] == 0:
